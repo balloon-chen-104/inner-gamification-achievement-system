72be9c2af41271ba492ac67c2c94aee48c50d18a
balloon.chen <balloon.chen@104.com.tw>
2020-05-06 15:41:36 +0800

    modified .gitignore

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UserSeeder::class);
+        // $this->call(GroupSeeder::class);
+        // $this->call(SettingSeeder::class);
+        // $this->call(CategorySeeder::class);
+        // $this->call(TaskSeeder::class);
+        // $this->call(BulletinSeeder::class);
+    }
+}
